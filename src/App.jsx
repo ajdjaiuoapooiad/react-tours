68e6aecfed6d87119ff35e1a7eccc5937f9b29cc
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-20 11:38:31 +0900

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
 const url = 'https://www.course-api.com/react-tours-project';
 
 const App = () => {
-  return <h2>Tours Starter</h2>;
+  const [ isloading,setIsLoading ] = useState(true)
+  const [ tours,setTours ] = useState([])
+
+  const fetchData = async () => {
+    setIsLoading(false)
+    try{
+       const response = await fetch(url) 
+       const tours = await response.json()
+       console.log(tours);
+       
+    }catch(error){
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  },[])
+
+  if(isloading){
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  )
 };
 export default App;
---
+++ src/Tours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tours = ({tours}) => {
+  return (
+    <div>Tours</div>
+  )
+}
+
+export default Tours
